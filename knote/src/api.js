64ec3ad6b125704b85f68e9cca9865874717e82e
WHITENOISE523 <withfreshforest@naver.com>
2026-05-17 18:07:14 +0900

    Restore main api integration

--- knote/src/api.js
+++ knote/src/api.js
@@ -1,9 +1,10 @@
-import axios from "axios";
+// src/api.js
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: 'http://localhost:8080/api/v1', // 백엔드 주소
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
 
